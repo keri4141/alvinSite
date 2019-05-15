d74d3b97d1b14e0f7b3d49f8825000eaad4a65b9
keri4141 <alvinchau2013@gmail.com>
2019-05-14 20:09:55 -0400

    Added basic particle with react setUp

--- my-site/src/App/App.js
+++ my-site/src/App/App.js
@@ -5,10 +5,49 @@ import {ReactComponent as Name} from '../assets/name.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.points =[];
+    this.points.push({
+      x: 100,
+      y: 100,
+      oldx:95,
+      oldy:95
+    })
+    this.state = { points: this.points };
+
+  }
+
+  componentDidMount() {
+    console.log(this.state.points);
+    requestAnimationFrame(this.updatePoints);
+  }
+
+  updatePoints = () => {
+    for(var i =0;i<this.points.length; i++) {
+      var p = this.points[i],
+        vx =p.x - p.oldx,
+        vy = p.y - p.oldy;
+    }
+    p.oldx = p.x;
+    p.oldy = p.y;
+    p.x+=vx;
+    p.y+=vy;
+    let points = {
+      x: p.x,
+      y: p.y,
+      oldx: p.oldx,
+      oldy: p.oldy
+    }
+    console.log("TEST");
+    this.setState({ points });
+    requestAnimationFrame(this.updatePoints);
+  }
+
   render() {
     return (
       <React.Fragment>
-      <Particle/>
+      <Particle points = {this.state.points}/>
         {/* <div className="name">
           <Name/>
         </div> */}
--- my-site/src/Particle/particle.js
+++ my-site/src/Particle/particle.js
@@ -1,39 +1,28 @@
 import React , {Component, PureComponent} from 'react';
 import ParticleClass from './particleClass';
-class Particle extends PureComponent {
+class Particle extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
     myRef = React.createRef();
-    frame = (mouse, ctx) => {
-        requestAnimationFrame(() => {this.frame(mouse,ctx)});
-        ctx.clearRect(0, 0, this.width, this.height);
-        for (var i = 0; i < this.particles.length; i++) {
-          this.particles[i].attract(mouse.x, mouse.y);
-          this.particles[i].integrate();
-          this.particles[i].draw(ctx);
-          console.log(this.particles);
-        }
-      }
+    renderPoints = (p) => {
+      this.ctx.clearRect(0,0, this.width, this.height);
+      this.ctx.beginPath();
+      this.ctx.arc(p.x, p.y, 5, 0, Math.PI *2);
+      this.ctx.fill();
+    }
       componentDidMount() {
         console.log(this.myRef);
          this.ctx = this.myRef.current.getContext('2d');
-         this.particles = [];
          this.width = this.myRef.current.width = window.innerWidth;
          this.height = this.myRef.current.height = window.innerHeight;
-         this.mouse = { x: this.width * 0.5, y: this.height * 0.5 };
-        for (var i = 0; i < 1; i++) {
-          this.particles[i] = new ParticleClass(Math.random() * this.width, Math.random() * this.height);
-        }
-        this.myRef.current.addEventListener('mousemove', this.onMousemove);
-        // requestAnimationFrame(() => {this.frame(this.mouse,this.ctx)});
-       
-      }
+      }     
       componentDidUpdate() {
-          console.log(this.ctx);
-        this.width = this.myRef.current.width = window.innerWidth;
-        this.height = this.myRef.current.height = window.innerHeight;
-        this.mouse = { x: this.width * 0.5, y: this.height * 0.5 };
-        requestAnimationFrame(() => {this.frame(this.mouse,this.ctx)});
-
-      }      
+        const {points} = this.props;
+        this.renderPoints(points);
+      }
     render() {
        
         return (
