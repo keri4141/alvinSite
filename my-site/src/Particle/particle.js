601fe1eab2415bb0c2a0715cc7d03d40c2f3932e
keri4141 <alvinchau2013@gmail.com>
2019-05-18 00:32:50 -0400

    Added multiple particles to follow mouse

--- my-site/src/Particle/particle.js
+++ my-site/src/Particle/particle.js
@@ -4,62 +4,84 @@ class Particle extends Component {
   constructor(props) {
     super(props);
     this.points =[];
-    this.points.push({
-      x: 100,
-      y: 100,
-      oldx:95,
-      oldy:95
-    })
+    // this.points.push({
+    //   x: 100,
+    //   y: 100,
+    //   oldx:95,
+    //   oldy:95
+    // })
     this.state = { points: this.points };
   }
 
     myRef = React.createRef();
-    renderPoints = (p) => {
-      this.ctx.clearRect(0,0, this.width, this.height);
-      this.ctx.beginPath();
-      this.ctx.arc(p.x, p.y, 5, 0, Math.PI *2);
-      this.ctx.fill();
-    }
+    // renderPoints = (p) => {
+    //   this.ctx.clearRect(0,0, this.width, this.height);
+    //   this.ctx.beginPath();
+    //   this.ctx.arc(p.x, p.y, 5, 0, Math.PI *2);
+    //   this.ctx.fill();
+    // }
         /*
       Will allow state to change hence triggering a re render on the component and on the canvas with new values
     */
       updatePoints = () => {
-        for(var i =0;i<this.points.length; i++) {
-          var p = this.points[i],
-            vx =p.x - p.oldx,
-            vy = p.y - p.oldy;
+        this.ctx.clearRect(0, 0, this.width, this.height);
+        for(let i =0;i<this.points.length;i++) {
+          this.points[i].attract(this.mouse.x,this.mouse.y);
+          this.points[i].integrate();
+         
+          this.points[i].draw(this.ctx);
+         
         }
-        p.oldx = p.x;
-        p.oldy = p.y;
-        p.x+=vx;
-        p.y+=vy;
-        let points = {
-          x: p.x,
-          y: p.y,
-          oldx: p.oldx,
-          oldy: p.oldy
-        }
-        console.log("TEST");
+        const points = Object.assign( {}, this.points);
+        // for(var i =0;i<this.points.length; i++) {
+        //   var p = this.points[i],
+        //     vx =p.x - p.oldx,
+        //     vy = p.y - p.oldy;
+        // }
+        // p.oldx = p.x;
+        // p.oldy = p.y;
+        // p.x+=vx;
+        // p.y+=vy;
+        // let points = {
+        //   x: p.x,
+        //   y: p.y,
+        //   oldx: p.oldx,
+        //   oldy: p.oldy
+        // }
         this.setState({ points });
         requestAnimationFrame(this.updatePoints);
+        
       }
       componentDidMount() {
         console.log(this.myRef);
+        this.width = this.myRef.current.width = window.innerWidth;
+        this.height = this.myRef.current.height = window.innerHeight;
+        this.mouse = { x: this.width * 0.5, y: this.height * 0.5 };
+        this.ctx = this.myRef.current.getContext('2d');
+        for (let i = 0; i < 150; i++) {
+          this.points[i] = new ParticleClass(Math.random() * this.width, Math.random() * this.height);
+
+        }
+       
+       
         
-         this.ctx = this.myRef.current.getContext('2d');
-         this.width = this.myRef.current.width = window.innerWidth;
-         this.height = this.myRef.current.height = window.innerHeight;
          requestAnimationFrame(this.updatePoints);
          
       }     
+      onMousemove=(e) => {
+        this.mouse.x = e.clientX;
+        this.mouse.y = e.clientY;
+      }
       componentDidUpdate() {
+        // this.ctx.clearRect(0, 0, this.width, this.height);
         const {points} = this.state;
-        this.renderPoints(points);
+        // console.log(points);
+        // this.renderPoints(points);
       }
     render() {
        
         return (
-            <canvas ref={this.myRef}>
+            <canvas onMouseMove={this.onMousemove} ref={this.myRef}>
             
             </canvas>
         );
--- my-site/src/Particle/particleClass.js
+++ my-site/src/Particle/particleClass.js
@@ -12,25 +12,29 @@ export default class ParticleClass {
         this.oldY = this.y;
         this.x += velocityX;
         this.y += velocityY;
+     
       };
       attract = (x, y)  => {
-        var dx = x - this.x;
-        var dy = y - this.y;
-        var distance = Math.sqrt(dx * dx + dy * dy);
-        x += dx / distance;
-        y += dy / distance;
+        
+        let dx = x - this.x;
+        let dy = y - this.y;
+       
+     
+        let distance = Math.sqrt(dx * dx + dy * dy);
+      
+        this.x += dx / distance;
+        this.y += dy / distance;
+       
       };
       draw = (ctx) => {
+        
         ctx.strokeStyle = '#444554';
         ctx.lineWidth = 2;
         ctx.beginPath();
         ctx.moveTo(this.oldX, this.oldY);
         ctx.lineTo(this.x, this.y);
         ctx.stroke();
-        console.log(ctx);
+        
       };
-      onMousemove=(e) => {
-        this.mouse.x = e.clientX;
-        this.mouse.y = e.clientY;
-      }
+      
 }
