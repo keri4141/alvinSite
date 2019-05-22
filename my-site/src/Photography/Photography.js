e172be950c3aeb162865706b2c0e47be4da8bf0e
Alvin Chau <alvinchau2013@gmail.com>
2019-05-21 22:18:22 -0400

    animated instagram svg

--- my-site/src/Photography/Photography.js
+++ my-site/src/Photography/Photography.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { ReactComponent as Instagram } from "../assets/instagram.svg";
 
 import "./Photography.css";
 
@@ -6,10 +7,8 @@ class Photography extends Component {
   render() {
     return (
       <div className=" photography container-fluid">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet,
-        purus vitae eleifend tristique, lorem magna volutpat orci, et vehicula
-        erat erat nec elit. Aenean posuere nunc ac cursus facilisis. Aenean vel
-        porta turpis, ut iaculis justo.
+      <Instagram/>
+        <img src="https://res.cloudinary.com/dsjx3wqn1/image/upload/v1558379572/VaticanOG-2_uk2pke.jpg" alt="test"></img>
       </div>
     );
   }
