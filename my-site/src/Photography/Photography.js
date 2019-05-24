6cf99af7ea4c283e52632444052d20ba6d1978d5
keri4141 <alvinchau2013@gmail.com>
2019-05-24 09:06:52 -0400

    Created firebase cloud function rest point and called endpoint sucessfully

--- my-site/src/Photography/Photography.js
+++ my-site/src/Photography/Photography.js
@@ -4,11 +4,23 @@ import { ReactComponent as Instagram } from "../assets/instagram.svg";
 import "./Photography.css";
 
 class Photography extends Component {
+  // componentDidMount() {
+  //   fetch("https://us-central1-alvinsiteapi-c68f6.cloudfunctions.net/images")
+  //     .then(function(response) {
+  //       return response.json();
+  //     })
+  //     .then(function(myJson) {
+  //       console.log(JSON.stringify(myJson));
+  //     });
+  // }
   render() {
     return (
       <div className=" photography container-fluid">
-      <Instagram/>
-        <img src="https://res.cloudinary.com/dsjx3wqn1/image/upload/v1558379572/VaticanOG-2_uk2pke.jpg" alt="test"></img>
+        <Instagram />
+        <img
+          src="https://res.cloudinary.com/dsjx3wqn1/image/upload/v1558379572/photography/VaticanOG-2_uk2pke.jpg"
+          alt="test"
+        />
       </div>
     );
   }
